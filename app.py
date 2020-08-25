39fd4dbb2e9e7f9ad03724606ce790a63c1647a7
Jovita Pinto <jovita964@gmail.com>
2020-08-25 00:39:55 -0400

    Uploading web pages files

--- app.py
+++ app.py
@@ -1,9 +1,25 @@
-from flask import Flask
+from flask import Flask, request, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World to Earth and back!'
+        return render_template('index.html')
+
+@app.route('/handle_data', methods=['POST'])
+def handle_data():
+    feeling = request.form['feeling']
+    print(feeling)
+
+    # insert model
+    #delete lines below after you add your model
+    import random
+    model_output = random.randint(0, 1)
+
+    output_feeling= "fine"
+    if model_output == 1:
+        output_feeling = "distressed"
+
+    return render_template('index.html', output=f"Our sources indicate - you feel: {output_feeling}")
 
 
 if __name__ =="__main__":
