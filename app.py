488e52727652b9f3b3e0ded9758f626c664681f4
Jovita Pinto <jovita964@gmail.com>
2020-08-27 20:20:25 -0400

    Fixed Indent

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-        return render_template('index.html')
+    return render_template('index.html')
 
 #@app.route('/handle_data', methods=['POST'])
 #def handle_data():
